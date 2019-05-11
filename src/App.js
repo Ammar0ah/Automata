926623088e0b314b51c7e88ae9b144cc826d777e
riadAlnajjar <riad.16.10@hotmail.com>
2019-05-11 17:53:35 +0300

    result

--- src/App.js
+++ src/App.js
@@ -5,21 +5,24 @@ import "./App.css";
 // import { Provider } from "react-redux";
 // import store from "./store";
 import Statement from "./Models/Statement";
+import { isArray } from "util";
 class App extends Component {
   state = {
     Qs: [{ q: [], isFinite: false }],
     Sigma: ["1", "2"],
+    testInput: ""
   };
   AddQs = () => {
     const newQs = [...this.state.Qs];
     newQs.push({ q: [], isFinite: false });
     this.setState({
       Qs: newQs
-    })
+    });
   };
-  AddState = (key, value, index) => {
+  AddState = (key, value, index, isFinite) => {
     const newQ = [...this.state.Qs];
     newQ[index].q = newQ[index].q.concat({ key: key, value: value });
+    newQ[index].isFinite = isFinite;
     this.setState({ Qs: newQ });
     console.log(newQ);
   };
@@ -29,42 +32,62 @@ class App extends Component {
     this.setState({ Qs: newQ });
   };
   check = (current, index) => {
+    if (isArray(index)) {
+      index = parseInt(index[0]);
+    }
     const Qs = this.state.Qs;
     let item = Qs[index];
-    console.log(item);
+    console.log(item, Qs, "index:", index, "cur:", current);
     let result = item.q.map(koko => {
       if (koko.key === current) return koko.value;
     });
+    if (isArray(result)) {
+      result = parseInt(result);
+    }
+    console.log("res", result);
     if (result) return result;
     else return -1;
   };
   isBelong = (current, chain) => {
-    let ch = String(chain)
     const Qs = this.state.Qs;
+    parseInt(current);
+    if (chain.length === 0) return Qs[current].isFinite;
+    let ch = String(chain);
+    console.log("ch", ch[0]);
+
     if (current === -1) return false;
-    else if (ch.length === 0) return Qs[current].isFinite;
-    else this.isBelong(this.check(ch[0], current), ch.slice(1, ch.length));
+    else {
+      const c = ch.slice(1, ch.length);
+      this.isBelong(this.check(ch[0], current), c);
+    }
   };
 
   inputValueChanged = event => {
     this.setState({ inputValue: event.target.value });
   };
 
-  
   render() {
     let St = this.state.Qs.map((_, index) => (
       <Statement key={index} clicked={this.AddState} index={index} />
     ));
     return (
-     <div>
-      
-       {St}
-      <button onClick={this.AddQs}>ADD NEW QS</button>
-      <input type="text" name="iiii" onChange={(e) => this.setState({testInput: e})}></input>
-        <button onClick={()=>{ let result = this.isBelong(0,this.state.testInput);alert("result: "+result)}}>Test</button>
+      <div>
+        {St}
+        <button onClick={this.AddQs}>ADD NEW QS</button>
+        <input
+          value={this.state.testInput}
+          onChange={e => this.setState({ testInput: e.currentTarget.value })}
+        />
+        <button
+          onClick={() => {
+            let result = this.isBelong(0, this.state.testInput);
+            alert("result: " + result);
+          }}
+        >
+          Test
+        </button>
       </div>
 
-      
       // <Provider store = {store}>
 
       // <main className="App">
--- src/Models/Statement.jsx
+++ src/Models/Statement.jsx
@@ -1,50 +1,59 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Statement extends Component {
-    state = {
-        inputKey: "",
-        inputValue: "",
-        inputIndex: ""
-    }
+  state = {
+    inputKey: "",
+    inputValue: "",
+    inputIndex: "",
+    isFinite: false
+  };
 
-    inputChanged = (e, type) => {
-        switch (type) {
-            case "key":
-                this.setState({ inputKey: e.target.value });
-                break;
-            case "value":
-                this.setState({ inputValue: e.target.value });
-                break;
-        }
+  inputChanged = (e, type) => {
+    switch (type) {
+      case "key":
+        this.setState({ inputKey: e.target.value });
+        break;
+      case "value":
+        this.setState({ inputValue: e.target.value });
+        break;
+      case "isFinite":
+        this.setState({ isFinite: !this.state.isFinite });
+        break;
     }
-    render() {
-        return (
-            <div >
-                <input
-                    onChange={e => this.inputChanged(e, "key")}
-                    value={this.state.inputKey}
-                />
-                <input
-                    onChange={e => this.inputChanged(e, "value")}
-                    value={this.state.inputValue}
-                />
-                <button
-                    onClick={() =>
-                        this.props.clicked(
-                            this.state.inputKey,
-                            this.state.inputValue,
-                            this.props.index
-                        )
-                    }
-                >
-                    SUBMIT
+  };
+  render() {
+    return (
+      <div>
+        <input
+          onChange={e => this.inputChanged(e, "key")}
+          value={this.state.inputKey}
+        />
+        <input
+          onChange={e => this.inputChanged(e, "value")}
+          value={this.state.inputValue}
+        />
+        <input
+          type="checkbox"
+          value={this.state.isFinite}
+          onChange={e => this.inputChanged(e, "isFinite")}
+        />
+        <button
+          onClick={() =>
+            this.props.clicked(
+              this.state.inputKey,
+              this.state.inputValue,
+              this.props.index,
+              this.state.isFinite
+            )
+          }
+        >
+          SUBMIT
         </button>
-                <h3>key: {this.state.inputKey}</h3>
-                <h3>Value: {this.state.inputValue}</h3>
-                <h3>Index: {this.state.inputIndex}</h3>
-            </div >
-        )
-    }
-};
+        <h3>key: {this.state.inputKey}</h3>
+        <h3>Value: {this.state.inputValue}</h3>
+      </div>
+    );
+  }
+}
 
 export default Statement;
